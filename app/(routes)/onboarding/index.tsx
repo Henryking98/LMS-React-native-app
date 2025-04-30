f4f65d5084b6fcb6f658ba34393f730fd9522fa2
Henry <94872110+Henryking98@users.noreply.github.com>
2025-04-30 16:32:10 +0100

    feat: added onboarding screen

--- app/(routes)/onboarding/index.tsx
+++ app/(routes)/onboarding/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import OnboardingScreen from "@/screens/onbaording/onboarding.screen";
-import { View, Text } from "react-native";
 
 export default function index() {
     return (
